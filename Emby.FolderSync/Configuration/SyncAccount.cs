0964b6a8962fc0dd5d63d4319983bbac2139f09b
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-08-30 18:16:54 -0400

    initial

---
+++ Emby.FolderSync/Configuration/SyncAccount.cs
@@ -0,0 +1,17 @@
+﻿
+namespace FolderSync.Configuration
+{
+    public class SyncAccount
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public bool EnableAllUsers { get; set; }
+        public string[] UserIds { get; set; }
+
+        public SyncAccount()
+        {
+            UserIds = new string[]{};
+        }
+    }
+}
